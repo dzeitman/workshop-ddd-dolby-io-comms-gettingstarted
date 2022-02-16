4eefd0f6336c74720d932369b1d41f550680855f
Braden Riggs <48164242+Briggs599@users.noreply.github.com>
2022-02-16 08:09:39 -0800

    Braden changes
    added some quality of life changes including toasts, device selection, simplified some code, updated color scheme.

--- www/scripts/client.js
+++ www/scripts/client.js
@@ -2,7 +2,7 @@ const avengersNames = ['Thor', 'Cap', 'Tony Stark', 'Black Panther', 'Black Wido
 let randomName = avengersNames[Math.floor(Math.random() * avengersNames.length)];
 
 // URL to our token-generator function
-const tokenServerURL = './api/token-generator';
+const tokenServerURL = 'https://zealous-bell-161b0d.netlify.app/api/token-generator';
 
 const main = async () => {
 
