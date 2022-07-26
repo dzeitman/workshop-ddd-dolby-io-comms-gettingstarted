d47a25fd19993b357310045231e80a7fa7423c48
Dan Zeitman (Dolby) <dan.zeitman@dolby.com>
2022-07-26 10:37:35 -0700

    add babylonjs

---
+++ www/scripts/vr.js
@@ -0,0 +1,48 @@
+var canvas = document.getElementById("renderCanvas");
+var engine = null;
+var scene = null;
+var sceneToRender = null;
+var createDefaultEngine = function () { return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true, disableWebGL2Support: false }); };
+
+
+
+const createScene =  () => {
+    const scene = new BABYLON.Scene(engine);
+
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+    camera.attachControl(canvas, true);
+
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
+
+    const box = BABYLON.MeshBuilder.CreateBox("box", {});
+
+    return scene;
+}
+
+
+window.initFunction = async function () {
+
+    // video = document.querySelector("video");
+    var asyncEngineCreation = async function () {
+        try {
+            return createDefaultEngine();
+        } catch (e) {
+            console.log("the available createEngine function failed. Creating the default engine instead");
+            return createDefaultEngine();
+        }
+    }
+
+    window.engine = await asyncEngineCreation();
+    if (!engine) throw 'engine should not be null.';
+    startRenderLoop(engine, canvas);
+    window.scene = createScene();
+};
+initFunction().then(() => {
+    scene.then(returnedScene => { sceneToRender = returnedScene; });
+
+});
+
+// Resize
+window.addEventListener("resize", function () {
+    engine.resize();
+});
